d7fe6f512fa7894b82c3a91789e66f74038692e8
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-04-09 23:56:20 +0200

    Changed storage location

--- htb/__init__.py
+++ htb/__init__.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).parent.parent
-STORAGE_DIR = ROOT_DIR / "storage"
+STORAGE_DIR = Path.home() / ".htb"
 
 if not STORAGE_DIR.exists():
     STORAGE_DIR.mkdir(exist_ok=True)
